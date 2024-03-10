06e5d4ff9f0ed231568cc9b07f8679041efdf2e7
Heedo Kim <devheedoo@gmail.com>
2024-03-11 02:38:05 +0900

    feat: Set routes - Home, Detail

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Detail from "./components/character/Detail";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/character/:id",
+    element: <Detail />,
+  },
+]);
 
 export default router;
