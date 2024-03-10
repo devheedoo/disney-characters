24bc7e065a30e466275ad4907ea36e2631ea9e8d
Heedo Kim <devheedoo@gmail.com>
2024-03-11 02:24:14 +0900

    feat: Set resetCSS

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./router";
+import { GlobalStyle } from "./GlobalStyle";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -12,6 +13,7 @@ const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
