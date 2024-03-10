bbea4cfe2428282e6f760282a1f36524a9a60003
Heedo Kim <devheedoo@gmail.com>
2024-03-11 02:50:49 +0900

    feat: Get Detail data

--- src/components/character/Detail.tsx
+++ src/components/character/Detail.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { CharacterSummary } from "../Home";
+import { useParams } from "react-router-dom";
+
+export interface CharacterDetails extends CharacterSummary {
+  /**
+   * @example ["The Computer Wore Tennis Shoes", "Now You See Him, Now You Don't", "The Strongest Man in the World"]
+   */
+  films: string[];
+  /**
+   * @example "https://disney.fandom.com/wiki/A.J._Arno"
+   */
+  sourceUrl: string;
+}
+
 const Detail = () => {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [details, setDetails] = useState<CharacterDetails | null>(null);
+
+  useEffect(() => {
+    fetch(`https://disney_api.nomadcoders.workers.dev/characters/${id}`)
+      .then((res) => res.json())
+      .then(setDetails);
+  }, []);
+
+  return (
+    <div>
+      <h1>Detail</h1>
+      {details && (
+        <ul key={details.id}>
+          <li>{details.name}</li>
+          <li>{details.imageUrl}</li>
+          <li>{details.films}</li>
+          <li>{details.sourceUrl}</li>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Detail;
